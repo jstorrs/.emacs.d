08d37a633b99b8ac86df114774687eaec148fd93
Judd Storrs <jstorrs@gmail.com>
2020-07-27 09:02:35 -0500

    Import .emacs.d from dotfiles

---
+++ init.el
@@ -0,0 +1,14 @@
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+;; Ensure "use-package" is installed.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Load emacs-init.org
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
